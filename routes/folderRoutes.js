9108607bf4c226c036067462a0bd95aeb14d22a2
Imseriious <samueledi80@hotmail.com>
2023-10-17 00:00:14 +0200

    update-folder-logic

--- routes/folderRoutes.js
+++ routes/folderRoutes.js
@@ -1,11 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const requireAuth = require('../middleware/requireAuth');
-const { createFolder, getUserFolders, deleteFolder, updateFolder } = require('../controllers/folderController');
+const requireAuth = require("../middleware/requireAuth");
+const {
+  createFolder,
+  getUserFolders,
+  deleteFolder,
+  updateFolder,
+  getFolderBookmarks,
+} = require("../controllers/folderController");
 
-router.post('/', requireAuth, createFolder);
-router.get('/', requireAuth, getUserFolders);
-router.delete('/:id', requireAuth, deleteFolder);
-router.put('/:id', requireAuth, updateFolder);
+router.post("/", requireAuth, createFolder);
+router.get("/", requireAuth, getUserFolders);
+router.delete("/:id", requireAuth, deleteFolder);
+router.put("/:id", requireAuth, updateFolder);
+router.get("/:id", requireAuth, getFolderBookmarks);
 
 module.exports = router;
