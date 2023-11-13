f908238c7612e6e9e6c98613d7c8dc56dd0dded7
Imseriious <samueledi80@hotmail.com>
2023-11-13 12:29:13 +0100

    favorite

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.set("trust proxy", 1);
 
 // Rate Limiting
 const limiter = rateLimit({
-  windowMs: 5 * 60 * 1000, // 5 minutes
+  windowMs: 1 * 60 * 1000, // 5 minutes
   max: 100, // limit each IP to 100 requests per windowMs
 });
 
