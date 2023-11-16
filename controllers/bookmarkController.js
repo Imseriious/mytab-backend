b19ade729b4554fdc29f3497e09a1373155350dd
Imseriious <samueledi80@hotmail.com>
2023-11-16 23:09:27 +0100

    import

--- controllers/bookmarkController.js
+++ controllers/bookmarkController.js
@@ -1,4 +1,7 @@
 const Bookmark = require("../models/bookmarkModel");
+const Category = require("../models/categoryModel");
+const Folder = require("../models/folderModel");
+
 const getBookmarkIconUrl = require("../utils/getFaviconFromUrl");
 const getWebsiteInfo = require("../utils/getWebsiteInfo");
 
@@ -124,9 +127,162 @@ const updateBookmark = async (req, res) => {
   }
 };
 
+const importBrowserBookmarks = async (req, res) => {
+  try {
+    const { browserBookmarks } = req.body;
+
+    const userBookmarks = browserBookmarks[0].children.filter(
+      (bookmarksList) => bookmarksList.children.length > 0
+    );
+
+    let categoriesToCreate = [];
+    let foldersToCreate = [];
+    let bookmarksToCreatePromises = [];
+
+    userBookmarks.forEach((bookmarkSections) => {
+      //Create categories from sections:
+      categoriesToCreate.push({
+        name: bookmarkSections.title,
+        userId: req.user._id,
+      });
+
+      bookmarkSections.children.forEach((bookmarkItem) => {
+        if (!bookmarkItem.children) {
+          //This are not folders
+          bookmarksToCreatePromises.push(
+            (async () => {
+              const iconUrl = await getBookmarkIconUrl(bookmarkItem.url);
+              let websiteDescription = await getWebsiteInfo(bookmarkItem.url);
+              const description =
+                websiteDescription.description ||
+                websiteDescription.title ||
+                "Missing description ... ";
+
+              return {
+                name: bookmarkItem.title,
+                url: bookmarkItem.url,
+                iconUrl: iconUrl,
+                description: description,
+                userId: req.user._id,
+              };
+            })()
+          ); // Again, notice the extra parentheses
+        } else {
+          //Create folders from Bookmark folders:
+          foldersToCreate.push({
+            name: bookmarkItem.title,
+            userId: req.user._id,
+            categoryName: bookmarkSections.title,
+          });
+
+          bookmarkItem.children.forEach((item) => {
+            bookmarksToCreatePromises.push(
+              (async () => {
+                const iconUrl = await getBookmarkIconUrl(item.url);
+                let websiteDescription = await getWebsiteInfo(item.url);
+                const description =
+                  websiteDescription.description ||
+                  websiteDescription.title ||
+                  "Missing description ... ";
+                return {
+                  name: item.title,
+                  url: item.url,
+                  iconUrl: iconUrl,
+                  description: description,
+                  userId: req.user._id,
+                  folderName: bookmarkItem.title,
+                };
+              })()
+            ); // Notice the extra parentheses
+          });
+        }
+      });
+    });
+
+    const bookmarksToCreate = await Promise.all(bookmarksToCreatePromises);
+
+    if (bookmarksToCreate.length > 0) {
+      const bookmarksWithoutFolder = bookmarksToCreate
+        .filter((bookmark) => !bookmark.folderName)
+        .map((bookmark) => ({
+          name: bookmark.name,
+          url: bookmark.url,
+          iconUrl: bookmark.iconUrl,
+          description: bookmark.description,
+          userId: bookmark.userId,
+        }))
+        .reverse(); // Reverse the array after filtering
+
+      try {
+        await Bookmark.insertMany(
+          bookmarksWithoutFolder
+        );
+      } catch (error) {
+        console.error("Error in bulk bookmark creation: ", error);
+      }
+    }
+
+    if (categoriesToCreate.length > 0) {
+      try {
+        const newCreatedCategories = await Category.insertMany(
+          categoriesToCreate
+        );
+        if (newCreatedCategories && foldersToCreate.length > 0) {
+          const formattedFolders = foldersToCreate.map((folder) => ({
+            categoryId: newCreatedCategories.filter(
+              (newCategory) => newCategory.name === folder.categoryName
+            )[0]._id,
+            name: folder.name,
+            userId: folder.userId,
+          }));
+          try {
+            const newCreatedFolders = await Folder.insertMany(formattedFolders);
+            const bookmarksFromFolders = bookmarksToCreate.filter(
+              (bookmark) => bookmark.folderName
+            );
+
+            if (bookmarksFromFolders.length > 0) {
+              const formattedBookmarksFromFolder = bookmarksFromFolders.map(
+                (bookmark) => ({
+                  name: bookmark.name,
+                  url: bookmark.url,
+                  iconUrl: bookmark.iconUrl,
+                  description: bookmark.description,
+                  userId: bookmark.userId,
+                  folderId: newCreatedFolders.filter(
+                    (newFolder) => newFolder.name === bookmark.folderName
+                  )[0]._id,
+                })
+              );
+              try {
+                await Bookmark.insertMany(formattedBookmarksFromFolder);
+              } catch (error) {
+                console.error(
+                  "Error in bulk bookmark (folder) creation: ",
+                  error
+                );
+              }
+            }
+          } catch (error) {
+            console.error("Error in bulk folder creation: ", error);
+          }
+        }
+      } catch (error) {
+        console.error("Error in bulk category creation: ", error);
+      }
+    }
+
+    res.status(200).json({ message: "Received bookmarks" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to import bookmark" });
+  }
+};
+
 module.exports = {
   addBookmark,
   getUserBookmarks,
   deleteBookmark,
   updateBookmark,
+  importBrowserBookmarks,
 };
--- routes/bookmarkRoutes.js
+++ routes/bookmarkRoutes.js
@@ -1,13 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { addBookmark, getUserBookmarks, deleteBookmark, updateBookmark } = require('../controllers/bookmarkController');
-const requireAuth = require('../middleware/requireAuth');
+const {
+  addBookmark,
+  getUserBookmarks,
+  deleteBookmark,
+  updateBookmark,
+  importBrowserBookmarks,
+} = require("../controllers/bookmarkController");
+const requireAuth = require("../middleware/requireAuth");
 
-router.post('/', requireAuth, addBookmark);
-router.get('/', requireAuth, getUserBookmarks);
-router.delete('/:id', requireAuth, deleteBookmark);
-router.put('/:id', requireAuth, updateBookmark);
-
-
+router.post("/", requireAuth, addBookmark);
+router.get("/", requireAuth, getUserBookmarks);
+router.delete("/:id", requireAuth, deleteBookmark);
+router.put("/:id", requireAuth, updateBookmark);
+router.post("/import_bookmarks", requireAuth, importBrowserBookmarks);
 
 module.exports = router;
