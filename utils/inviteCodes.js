5dea952dfbab97b7f60cbde3fa538617ec55b1c1
Samuel Pop <47713133+Imseriious@users.noreply.github.com>
2024-12-10 14:11:45 +0100

    Update inviteCodes.js

--- utils/inviteCodes.js
+++ utils/inviteCodes.js
@@ -1,16 +1,5 @@
 const inviteCodes = [
-  {
-    email: "sam@millerio.com",
-    code: "sleektab.sam",
-    userType: "guser",
-    socialPlatform: "1",
-  },
-  {
-    email: "nc.pewfs@hotmail.com",
-    code: "sleektab.SeCrEtCoDe.69",
-    userType: "guser",
-    socialPlatform: "pewfs",
-  },
+
 ];
 
 module.exports = { inviteCodes };
